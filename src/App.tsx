d6f1a437d465baddb4aeb06f1e531b60bb4f343a
LwinMat <lymateo-lopez@myseneca.ca>
2026-05-30 22:04:20 -0400

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EmailVerification from './pages/email-verification';
 import ResetPassword from './pages/reset-password';
